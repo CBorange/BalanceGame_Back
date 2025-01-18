aa1e01291aefcdfe918cf1a79c4e0134ebe598d4
CBorange <xocnsdl2013@gmail.com>
2025-01-18 15:17:45 +0900

    Project Init

---
+++ src/main/java/com/saks/balance/backend/BalanceBackApplication.java
@@ -0,0 +1,11 @@
+package com.saks.balance.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BalanceBackApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(BalanceBackApplication.class, args);
+	}
+}
